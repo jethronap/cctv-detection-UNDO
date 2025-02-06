b48a841d46bebbcdbacc9017d6aa21bb815813ee
jethronap <efthioa@gmail.com>
2025-02-06 16:55:36 +0200

    Updated docstrings

--- downloader.py
+++ downloader.py
@@ -7,6 +7,12 @@ from src.image_scrapers import capture_screenshots
 
 
 def image_downloader(csv_path: Path, output_dir: Path):
+    """
+    Loads camera data from a csv, visits links, takes screenshots and persists in dedicated folder.
+    :param csv_path: The Path object to csv file.
+    :param output_dir: The Path object to output directory.
+    :return:
+    """
     cameras = load_camera_data(csv_path)
     logger.info(f"Urls found: {len(cameras)}")
     capture_screenshots(cameras, output_dir)
--- src/data_loaders.py
+++ src/data_loaders.py
@@ -4,7 +4,11 @@ import pandas as pd
 
 
 def load_camera_data(csv_path: Path) -> List[Tuple[float, float, str]]:
-    """Load camera data from a CSV file and extract valid unique URLs with coordinates."""
+    """
+    Load camera data from a CSV file and extract valid unique URLs with coordinates.
+    :param csv_path: The Path object to the csv file.
+    :return: A list of camera information: lon, lat, url.
+    """
     df = pd.read_csv(csv_path)
     df = df.dropna(subset=["url"])  # Remove rows where URL is missing
     df = df.drop_duplicates(subset=["url"])  # Keep only unique URLs
