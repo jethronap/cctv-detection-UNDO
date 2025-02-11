88d3863b03c64ed81dfe60eea3e0f55e5a189ec3
jethronap <efthioa@gmail.com>
2025-02-11 15:10:03 +0200

    Updated config

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 from pathlib import Path
 
-DATASETS: Path = Path("datasets")
+# ------------------- Training config -------------------#
+PROJECT_ROOT: Path = Path(__file__).resolve().parent
+DATASETS: Path = PROJECT_ROOT / "datasets"
+IMAGES_DIR: Path = DATASETS / "images"
+LABELS_DIR: Path = DATASETS / "labels"
+TRAIN_RATIO: float = 0.7
+VAL_RATIO: float = 0.3
+BATCH_SIZE: int = 4
+
+# ------------------- Scraper config -------------------#
 CSV_FILE: Path = Path("../../datasets/cctv-aware-jyvaskyla.csv")
 OUTPUT_DIR: Path = Path(
     "../datasets/screenshots"
